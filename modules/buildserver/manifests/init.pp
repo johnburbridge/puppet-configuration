6f38fbc9f244bb20928f71dd08a71aea27d1e7a5
John Burbridge <johnburbridge@gmail.com>
2012-07-26 18:45:37 -0700

    adding build server configuration module

---
+++ modules/buildserver/manifests/init.pp
@@ -0,0 +1,53 @@
+# Class: buildserver
+#
+# This module manages the secrets for the build.metabuild.net server 
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class buildserver {
+    
+    file { '/usr/share/tomcat7/.aws':
+        ensure => directory,
+        owner => 'tomcat7',
+        group => 'tomcat7',
+        mode => '700',
+        require => Package['tomcat7'],
+    }
+    
+    file { '/usr/share/tomcat7/.aws/aws.properties':
+        content => template('buildserver/aws.properties.erb'),
+        checksum => md5,
+        replace => false,
+        owner => 'tomcat7',
+        group => 'tomcat7',
+        mode => '640',
+        ensure => present,
+        require => File['/usr/share/tomcat7/.aws'],
+    }
+
+    file { '/usr/share/tomcat7/.gradle':
+        ensure => directory,
+        owner => 'tomcat7',
+        group => 'tomcat7',
+        mode => '700',
+        require => Package['tomcat7'],
+    }
+
+    file { '/usr/share/tomcat7/.gradle/gradle.properties':
+        content => template('buildserver/gradle.properties.erb'),
+        checksum => md5,
+        replace => false,
+        owner => 'tomcat7',
+        group => 'tomcat7',
+        mode => '640',
+        ensure => present,
+        require => File['/usr/share/tomcat7/.gradle'],
+    }
+}
