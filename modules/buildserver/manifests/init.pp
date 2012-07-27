9f9897ba0d171409868c0aa89335306b549e5f6f
John Burbridge <johnburbridge@gmail.com>
2012-07-27 01:32:51 -0700

    fixing git perms issues with jenkins / ubuntu

--- modules/buildserver/manifests/init.pp
+++ modules/buildserver/manifests/init.pp
@@ -13,14 +13,43 @@
 # [Remember: No empty lines between comments and class definition]
 class buildserver {
     
+    $jenkins_download_url = "http://mirrors.jenkins-ci.org/war/latest/jenkins.war"
+    
+    exec { "wget $jenkins_download_url":
+        cwd => "/var/lib/tomcat7/webapps",
+        creates => "/var/lib/tomcat7/webapps/jenkins.war",
+        path => ["/usr/bin"],
+        require => File['/usr/share/tomcat7/.jenkins'],
+    }
+    
+    file { '/usr/share/tomcat7/.jenkins':
+        ensure => directory,
+        owner => 'tomcat7',
+        group => 'tomcat7',
+        mode => '700',
+        require => Package['tomcat7'],
+    }
+
+    
     # need to make sure the .git folder belongs to tomcat so that 
     # the jenkins git plug-in can write to it
     file { '/usr/share/tomcat7/.git':
         ensure => directory,
         owner => 'tomcat7',
         group => 'tomcat7',
-        mode => '700',
+        mode => '755',
         require => Package['tomcat7'],
+    }
+    
+    file { '/usr/share/tomcat7/.git/config':
+        content => template('buildserver/git-config.erb'),
+        checksum => md5,
+        replace => false,
+        owner => 'tomcat7',
+        group => 'tomcat7',
+        mode => '644',
+        ensure => present,
+        require => File['/usr/share/tomcat7/.git'],
     }
     
     file { '/usr/share/tomcat7/.aws':
