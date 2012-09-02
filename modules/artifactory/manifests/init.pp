d68a5fe5680ee984bf8f11c99926e32f2badac41
John Burbridge <johnburbridge@gmail.com>
2012-09-02 12:08:39 -0700

    changing download of artifactory.war to remote repo

--- modules/artifactory/manifests/init.pp
+++ modules/artifactory/manifests/init.pp
@@ -11,17 +11,24 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class artifactory  {
+class artifactory ($artifactory_download_url = "http://repo.metabuild.net/artifactory/tools/artifactory/2.6.3/artifactory.war") {
 
     include tomcat
+
+    exec { 'download':
+        command => "wget $artifactory_download_url",
+        cwd => "/var/lib/tomcat7/webapps",
+        creates => "/var/lib/tomcat7/webapps/artifactory.war",
+        path => ["/usr/bin"],
+        require => File['/usr/share/tomcat7/.artifactory'],
+    }
     
     file { '/var/lib/tomcat7/webapps/artifactory.war':
         ensure => present,
-        source => '/tmp/artifactory.war',
         owner => 'tomcat7',
         group => 'tomcat7',
-        mode => '700',
-        require => File['/usr/share/tomcat7/.artifactory'],
+        mode => '644',
+        require => Exec['download'],
     }
     
     file { '/usr/share/tomcat7/.artifactory':
