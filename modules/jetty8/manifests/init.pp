bf2b7ae3f315dc4a437781fe1ab3dded83f1700c
John Burbridge <johnburbridge@gmail.com>
2012-09-03 14:38:46 -0700

    Replacing stock root context with www.war

--- modules/jetty8/manifests/init.pp
+++ modules/jetty8/manifests/init.pp
@@ -11,7 +11,12 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class jetty8 ($jetty8_download_url = 'http://dist.codehaus.org/jetty/deb/8.1.5.v20120716/jetty-deb-8.1.5.v20120716.deb') {
+class jetty8 (
+    $jetty_version = '8.1.5.v20120716',
+    $jetty_port = 8081
+) {
+
+    $jetty_download_url = "http://dist.codehaus.org/jetty/deb/${jetty_version}/jetty-deb-${jetty_version}.deb"
 
     file { '/root/repo':
         ensure => directory,
@@ -20,10 +25,10 @@ class jetty8 ($jetty8_download_url = 'http://dist.codehaus.org/jetty/deb/8.1.5.v
         mode => '755',
     }
     
-    exec { 'download_jetty8':
-        command => "wget $jetty8_download_url",
+    exec { 'download_jetty':
+        command => "wget $jetty_download_url",
         cwd => "/root/repo",
-        creates => "/root/repo/jetty-deb-8.1.5.v20120716.deb",
+        creates => "/root/repo/jetty-deb-${jetty_version}.deb",
         path => ["/usr/bin"],
         require => File['/root/repo'],
     }
@@ -31,8 +36,23 @@ class jetty8 ($jetty8_download_url = 'http://dist.codehaus.org/jetty/deb/8.1.5.v
     package { 'jetty-hightide-server':
         provider => dpkg,
         ensure => latest,
-        require => Exec['download_jetty8'],
-        source => '/root/repo/jetty-deb-8.1.5.v20120716.deb',
+        require => [Exec['download_jetty'], Package['openjdk-6-jdk']],
+        source => "/root/repo/jetty-deb-${jetty_version}.deb",
+    }
+    
+    package { 'openjdk-6-jdk': 
+        ensure => latest,
+    }
+    
+    file { "/opt/jetty/start.ini":
+        owner => 'root',
+        require => Package['jetty-hightide-server'],
+        notify => Service['jetty'],
+        content => template('jetty8/start.ini.erb')
+    }
+    
+    file { "/opt/jetty/contexts/hightide.xml":
+        ensure => absent
     }
     
     service { 'jetty':
