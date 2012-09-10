4dbe9f4a4d51c7edcdd9e780636c9d70bf42a106
John Burbridge <johnburbridge@gmail.com>
2012-09-09 21:27:02 -0700

    Updating wwwserver to download files from puppet master

--- modules/wwwserver/manifests/init.pp
+++ modules/wwwserver/manifests/init.pp
@@ -13,9 +13,7 @@
 # [Remember: No empty lines between comments and class definition]
 class wwwserver {
 
-    $repo_url = 'http://repo.metabuild.net/artifactory/libs-snapshot-local'
-    $www_version = '1.1-SNAPSHOT'
-    $www_download_url = "${repo_url}/org/metabuild/www/${www_version}/www-${www_version}.war"
+    $www_version = $::www_version
     
     class { 'jetty8':
         jetty_port => 8085,
@@ -49,29 +47,21 @@ class wwwserver {
         require => Class['jetty8'],
     }
     
-    exec { 'download_www_war':
-        command => "wget -O www.war ${www_download_url}",
-        cwd => "/tmp",
-        creates => "/tmp/www.war",
-        path => ["/usr/bin"],
-        require => Class['aws_client'],
-        notify => Exec['purge_old_root'],
-    }
-    
     exec { 'purge_old_root':
         command => 'rm -Rf root', 
         cwd => '/opt/jetty/webapps',
         path => ['/bin'],
         refreshonly => true
     }
-    
+
     file { '/opt/jetty/webapps/root.war':
         ensure => present,
         owner => 'root',
         group => 'root',
         mode => '644',
-        source => '/tmp/www.war',
-        require => Exec['download_www_war'],
+        source => "puppet:///modules/wwwserver/www-${www_version}.war",
+        require => Exec['purge_old_root'],
+        notify => Service['jetty'],
     }
     
 }
